3a76d00956566eee84a550a50df4c4212e815eac
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-09-25 17:29:10 +0900

    프레임 크기가 다른 스프라이트 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -15,15 +15,20 @@ keroro_left_down = load_image('keroro_left_down.png')
 keroro_right_up = load_image('keroro_right_up.png')
 keroro_left_up = load_image('keroro_left_up.png')
 
-def motion():
-    global running, dir_x, dir_y
+keroro_kill = load_image('keroro_kill.png')
+
+def animation():
+    global running, dir_x, dir_y, killing
+    global frame, x, y
     events = get_events()
 
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+            killing = False
         elif event.key == SDLK_ESCAPE:
             running = False
+            killing = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir_x += 1
@@ -33,6 +38,18 @@ def motion():
                 dir_y += 1
             if event.key == SDLK_DOWN:
                 dir_y -= 1
+            if event.key == SDLK_k:
+                running = False
+                killing = True
+                frame = 0
+            if event.key == SDLK_r:
+                running = True
+                killing = False
+                x = TUK_WIDTH // 2
+                y = TUK_HEIGHT // 2
+                frame = 0
+                dir_x = 0
+                dir_y = 0
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 dir_x -= 1
@@ -44,11 +61,14 @@ def motion():
                 dir_y += 1
 
 running = True
+killing = False
 x = TUK_WIDTH // 2
 y = TUK_HEIGHT // 2
 frame = 0
 dir_x = 0
 dir_y = 0
+
+
 
 while running:
     clear_canvas()
@@ -72,15 +92,39 @@ while running:
         keroro_left_down.clip_draw(frame * 260, 0, 260, 350, x, y, 130, 195)
     elif dir_x > 0 and dir_y < 0 : # right_down
         keroro_right_down.clip_draw(frame * 260, 0, 260, 350, x, y, 130, 195)
-
     update_canvas()
-    motion()
+    animation()
     frame = (frame + 1) % 4
     x += dir_x * 10
-    y += dir_y * 10
+    y += dir_y * 15
     if x > TUK_WIDTH - 65 or x < 65 :
         x -= dir_x * 10
     if y > TUK_HEIGHT - 98 or y < 98:
-        y -= dir_y * 10
+        y -= dir_y * 15
     delay(0.05)
-    
+
+while killing:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    if frame == 0:
+        keroro_kill.clip_draw(0, 0, 175, 160, x, y, 130, 195)
+    elif frame == 1:
+        keroro_kill.clip_draw(175, 0, 139, 139, x, y, 100, 100)
+    elif frame == 2:
+        keroro_kill.clip_draw(350, 0, 112, 122, x, y, 80, 80)
+    elif frame == 3:
+        keroro_kill.clip_draw(525, 0, 105, 100, x, y, 70, 70)
+    elif frame == 4:
+        keroro_kill.clip_draw(700, 0, 89, 78, x, y, 60, 60)
+    elif frame == 5:
+        keroro_kill.clip_draw(875, 0, 72, 67, x, y, 50, 50)
+    elif frame == 6:
+        keroro_kill.clip_draw(1050, 0, 50, 50, x, y, 40, 40)
+    elif frame == 7:
+        keroro_kill.clip_draw(1225, 0, 34, 33, x, y, 20, 20)
+    update_canvas()
+    animation()
+    frame += 1
+    delay(0.05)
+
+close_canvas()
